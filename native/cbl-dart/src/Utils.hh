7b9cc83e64f7475cfedadccacd71791c0aa034b3
Gabriel Terwesten <gabriel@terwesten.net>
2021-07-10 00:47:20 +0200

    fix formatting

--- native/cbl-dart/src/Utils.hh
+++ native/cbl-dart/src/Utils.hh
@@ -10,7 +10,7 @@
  * Returns the value of the given Dart CObject, wich is expected to be either an
  * int32 or int64, as an int64_t.
  */
-int64_t CBLDart_CObject_getIntValueAsInt64(Dart_CObject *object);
+int64_t CBLDart_CObject_getIntValueAsInt64(Dart_CObject* object);
 
 void CBLDart_CObject_SetEmptyArray(Dart_CObject* object);
 
