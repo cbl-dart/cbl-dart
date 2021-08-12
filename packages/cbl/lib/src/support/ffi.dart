ddd96e918bbc338b3413fa756939192be575cde4
Gabriel Terwesten <gabriel@terwesten.net>
2021-08-12 19:36:20 +0200

    internal: add `IsolateWorker`

--- packages/cbl/lib/src/support/ffi.dart
+++ packages/cbl/lib/src/support/ffi.dart
@@ -1,11 +1,28 @@
-import 'package:cbl_ffi/cbl_ffi.dart';
+import 'package:cbl_ffi/cbl_ffi.dart' as ffi;
 
-late final CBLBindings cblBindings = () {
-  final bindings = CBLBindings.maybeInstance;
+import '../couchbase_lite.dart';
+
+Libraries? _libraries;
+
+Libraries get libraries {
+  if (_libraries == null) {
+    _throwCblNotInitializedError();
+  }
+  return _libraries!;
+}
+
+set libraries(Libraries value) => _libraries = value;
+
+late final ffi.CBLBindings cblBindings = () {
+  final bindings = ffi.CBLBindings.maybeInstance;
   if (bindings == null) {
-    throw StateError(
-      'CouchbaseLite.init must be called before using the cbl library.',
-    );
+    _throwCblNotInitializedError();
   }
   return bindings;
 }();
+
+Never _throwCblNotInitializedError() {
+  throw StateError(
+    'CouchbaseLite.init must be called before using the cbl library.',
+  );
+}
---
+++ packages/cbl_e2e_tests/lib/src/service/isolate_worker_test.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:cbl/src/service/isolate_worker.dart';
+
+import '../../test_binding_impl.dart';
+import '../test_binding.dart';
+
+void main() {
+  setupTestBinding();
+
+  group('IsolateWorker', () {
+    test('start and stop worker', () async {
+      final worker = testWorker();
+
+      await worker.start();
+      await worker.stop();
+    });
+
+    test('starting worker which is not in initial state throws', () async {
+      final worker = testWorker();
+
+      await worker.start();
+
+      expect(() => worker.start(), throwsStateError);
+
+      await worker.stop();
+
+      expect(() => worker.start(), throwsStateError);
+    });
+
+    test('stopping worker which has not be started throws', () async {
+      final worker = testWorker();
+
+      expect(() => worker.stop(), throwsStateError);
+
+      await worker.start();
+      await worker.stop();
+
+      expect(() => worker.stop(), throwsStateError);
+    });
+  });
+}
+
+IsolateWorker testWorker() {
+  return IsolateWorker(delegate: TestWorkerDelegate());
+}
+
+class TestWorkerDelegate extends IsolateWorkerDelegate {
+  @override
+  FutureOr<void> dispose() {}
+
+  @override
+  FutureOr<void> initialize() {}
+}
