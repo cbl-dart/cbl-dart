b3629bc4059a37989f5ae54cd41b8e6027c0e4e8
Gabriel Terwesten <gabriel@terwesten.net>
2021-09-01 16:39:35 +0200

    build: statically link c++ std lib for linux and strip symbols

--- native/tools/build_unix.sh
+++ native/tools/build_unix.sh
@@ -9,6 +9,11 @@ nativeDir="$(cd "$toolsDir/.." && pwd)"
 projectDir="$(cd "$nativeDir/.." && pwd)"
 buildDir="$projectDir/build/unix"
 libDir="$projectDir/build/unix/lib"
+CBLDART_STATIC_CXX="OFF"
+
+if [[ "$(uname)" == "Linux" ]]; then
+    CBLDART_STATIC_CXX="ON"
+fi
 
 # === Commands ===
 
@@ -38,6 +43,7 @@ function _configure() {
         -DCMAKE_INSTALL_PREFIX="$buildDir/install" \
         -DCMAKE_C_COMPILER_LAUNCHER=ccache \
         -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+        -DCBLDART_STATIC_CXX="$CBLDART_STATIC_CXX" \
         -DCMAKE_BUILD_TYPE="$buildType" \
         "$nativeDir"
 }
@@ -45,9 +51,11 @@ function _configure() {
 function _build() {
     cmake --build "$buildDir"
     _copyToLib
+    _stripSymbols
 }
 
 function _copyToLib() {
+    rm -rf "$libDir"
     mkdir -p "$libDir"
 
     cp -P \
@@ -56,8 +64,46 @@ function _copyToLib() {
         "$libDir"
 }
 
+function _stripSymbols() {
+    case "$(uname)" in
+    Linux)
+        _stripSymbolsLinux "$libDir/libcblite.so".*.*.*
+        _stripSymbolsLinux "$libDir/libcblitedart.so"
+        ;;
+    Darwin)
+        _stripSymbolsMacOS "$libDir/libcblite".*.*.*.dylib
+        _stripSymbolsMacOS "$libDir/libcblitedart.dylib"
+        ;;
+    *)
+        echo "$OSTYPE is not a unix OS"
+        exit 1
+        ;;
+    esac
+}
+
+function _stripSymbolsLinux() {
+    local libraryPath="$1"
+    local libraryDir="$(dirname "$libraryPath")"
+    local libraryName="$(basename "$libraryPath")"
+    local librarySym="$libraryName.sym"
+    cd "$libraryDir"
+    objcopy --only-keep-debug "$libraryName" "$librarySym"
+    strip --strip-unneeded "$libraryName"
+    objcopy --add-gnu-debuglink "$librarySym" "$libraryName"
+}
+
+function _stripSymbolsMacOS() {
+    local libraryPath="$1"
+    dsymutil "${libraryPath}" -o "$libraryPath.dSYM"
+    strip -x "${libraryPath}"
+}
+
 function _createLinksForDev() {
     cd "$projectDir/packages/cbl_e2e_tests_standalone_dart"
+    rm -f lib
+    ln -s "$libDir"
+
+    cd "$projectDir/packages/cbl_flutter/linux"
     rm -f lib
     ln -s "$libDir"
 }
