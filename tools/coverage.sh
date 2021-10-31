63f7a212e12e93524d6b4771c51f947f24073c6f
Gabriel Terwesten <gabriel@terwesten.net>
2021-10-31 21:01:58 +0100

    ci: make coverage data more accurate (#207)

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 # Converts the dart coverage output for a package to lcov.
 #
@@ -11,17 +11,20 @@ set -e
 # The output is written to `$PACKAGE_DIR$/coverage/lcov.info`.
 function dartToLcov() {
     local packageDir="$1"
-    local coverageDir="$packageDir/coverage"
+    local coverageDir="coverage"
     local input="$coverageDir/dart"
     local output="$coverageDir/lcov.info"
-    local packagesFile="$packageDir/.packages"
+    local packagesFile=".packages"
 
     dart pub global activate coverage
 
+    # `format_coverage` only works when executed from the root of the package.
+    cd "$packageDir"
+
     format_coverage \
-        -i "$input" \
-        -l \
-        -o "$output" \
+        --in "$input" \
+        --lcov \
+        --out "$output" \
         --packages "$packagesFile"
 }
 
