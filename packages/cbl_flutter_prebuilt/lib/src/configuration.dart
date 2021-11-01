8a003a46430a678125676c4685631a9deb0e81e8
Gabriel Terwesten <gabriel@terwesten.net>
2021-11-01 15:48:32 +0100

    chore: cut `cbl_flutter_ce|ee` release 1.0.0-beta.1

--- packages/cbl_flutter_prebuilt/lib/src/configuration.dart
+++ packages/cbl_flutter_prebuilt/lib/src/configuration.dart
@@ -11,7 +11,7 @@ final packageConfigurations = [
   for (final edition in Edition.values)
     PackageConfiguration(
       name: packageNames[edition]!,
-      version: '1.0.0-beta.0',
+      version: '1.0.0-beta.1',
       edition: edition,
       couchbaseLiteC: const LibraryInfo(
         version: '3.0.0',
@@ -20,7 +20,7 @@ final packageConfigurations = [
       ),
       couchbaseLiteDart: const LibraryInfo(
         version: '1.0.0',
-        release: '1.0.0-beta.1',
+        release: '1.0.0-beta.2',
       ),
     )
 ];
