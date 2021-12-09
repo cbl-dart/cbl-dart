441aa2cb5b4d8b6c0f40e0347a0f7231ee81c6d9
Gabriel Terwesten <gabriel@terwesten.net>
2021-12-09 09:05:17 +0100

    feat: no longer require manual plugin registration for mobile
    Closes #199

--- packages/cbl_e2e_tests_flutter/integration_test/test_binding_impl.dart
+++ packages/cbl_e2e_tests_flutter/integration_test/test_binding_impl.dart
@@ -4,7 +4,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:cbl_flutter/cbl_flutter.dart';
-import 'package:cbl_flutter_local/cbl_flutter_local.dart';
 import 'package:flutter_test/flutter_test.dart' as ft;
 import 'package:integration_test/integration_test.dart';
 import 'package:path_provider/path_provider.dart';
@@ -12,11 +11,6 @@ import 'package:path_provider/path_provider.dart';
 import 'cbl_e2e_tests/test_binding.dart';
 
 void setupTestBinding() {
-  // TODO(blaugold): remove when mobile supports registration of dart plugins
-  if (Platform.isIOS || Platform.isAndroid) {
-    CblFlutterLocal.registerWith();
-  }
-
   final widgetBinding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
       as IntegrationTestWidgetsFlutterBinding;
   // ignore: cascade_invocations
