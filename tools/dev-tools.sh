9c5c9016b595a3d469210ae32655643e07828d92
Gabriel Terwesten <gabriel@terwesten.net>
2022-05-12 11:58:41 +0200

    build: activate `pubspec_overrides.yaml` in Melos (#363)

--- tools/dev-tools.sh
+++ tools/dev-tools.sh
@@ -18,16 +18,6 @@ scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 projectDir="$(cd "$scriptDir/.." && pwd)"
 nativeDir="$projectDir/native"
 packagesDir="$projectDir/packages"
-nonMelosDartPackages=(
-    cbl_e2e_tests
-    cbl_e2e_tests_standalone_dart
-    cbl_flutter_prebuilt
-)
-nonMelosFlutterPackages=(
-    cbl_e2e_tests_flutter
-    cbl_flutter_local
-    cbl_flutter_prebuilt_e2e_tests
-)
 couchbaseLiteCPrebuiltDir="$nativeDir/vendor/couchbase-lite-C-prebuilt"
 couchbaseLiteCRelease="$(cat "$nativeDir/CouchbaseLiteC.release")"
 couchbaseLiteDartDir="$nativeDir/couchbase-lite-dart"
@@ -160,50 +150,15 @@ function prepareNativeLibraries() {
 
 function bootstrapPackage() {
     local package="$1"
-    local packageDir="$packagesDir/$package"
-
-    # If package is nonMelosDartPackages, bootstrap it with dart.
-    if [[ " ${nonMelosDartPackages[@]} " =~ " $package " ]]; then
-        echo "Bootstrapping $package with dart"
-        dart pub get -C "$packageDir"
-        return
-    fi
-
-    # If package is nonMelosFlutterPackages, bootstrap it with flutter.
-    if [[ " ${nonMelosFlutterPackages[@]} " =~ " $package " ]]; then
-        echo "Bootstrapping $package with flutter"
-        cd "$packageDir"
-        flutter pub get
-        return
-    fi
-
-    # Package must be managed by melos, so bootstrap it with melos.
-    echo "Bootstrapping $package with melos"
-    # We first need to run flutter pub get in the package to generated flutter
-    # sepecific files.
-    cd "$packageDir"
-    flutter pub get
     $melosBin bootstrap --scope "$package"
 }
 
 function bootstrap() {
-    # We first need to run flutter pub get in the melos managed flutter package,
-    # to generated flutter sepecific files.
-    $melosBin run flutter:pubGet
     $melosBin bootstrap
-
-    for package in ${nonMelosDartPackages[@]} ${nonMelosFlutterPackages[@]}; do
-        bootstrapPackage $package
-    done
 }
 
 function analyze() {
     $melosBin run analyze
-
-    for package in ${nonMelosDartPackages[@]} ${nonMelosFlutterPackages[@]}; do
-        cd "$packagesDir/$package"
-        dart analyze --fatal-infos
-    done
 }
 
 "$@"
