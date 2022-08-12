ee922f1cbc0a2bdaa416de5e5676cb93eca05a06
Gabriel Terwesten <gabriel@terwesten.net>
2022-08-12 21:39:39 +0200

    build: install required versions of Android native toolchain

---
+++ native/tools/android-sdk.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Utilities to install and manage Android SDK.
+
+set -e
+
+ndkVersion="21.4.7075529"
+cmakeVersion="3.18.1"
+defaultSdkLocation=("$HOME/Android/Sdk" "$HOME/Library/Android/sdk")
+sdkHome="$ANDROID_HOME"
+
+# Look for Android SDK in default locations
+if [ -z "$sdkHome" ]; then
+    for location in "${defaultSdkLocation[@]}"; do
+        if [ -d "$location" ]; then
+            sdkHome="$location"
+            break
+        fi
+    done
+
+    if [ -z "$sdkHome" ]; then
+        echo "Could not find Android SDK."
+        exit 1
+    fi
+fi
+
+
+function installNativeToolchain() {
+    $sdkHome/cmdline-tools/latest/bin/sdkmanager --install "ndk;$ndkVersion"
+    $sdkHome/cmdline-tools/latest/bin/sdkmanager --install "cmake;$cmakeVersion"
+}
+
+"$@"
