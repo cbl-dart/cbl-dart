34eef27e125d2c965b175c76bed9aeb5f617cfbb
Gabriel Terwesten <gabriel@terwesten.net>
2022-11-14 08:44:28 +0100

    Cleanup logging and use published version of `archive`

--- packages/cbl_dart/lib/src/utils.dart
+++ packages/cbl_dart/lib/src/utils.dart
@@ -1,4 +1,37 @@
+import 'dart:async';
+
 import 'package:logging/logging.dart';
+
+/// Whether to enable logging to debug issues specific to `cbl_dart`.
+bool get enableDebugLogging => _debugLoggingSubscription != null;
+
+// ignore: avoid_positional_boolean_parameters
+set enableDebugLogging(bool value) {
+  if (enableDebugLogging == value) {
+    return;
+  }
+  if (value) {
+    _enableDebugLogging();
+  } else {
+    _disableDebugLogging();
+  }
+}
+
+StreamSubscription? _debugLoggingSubscription;
+
+void _enableDebugLogging() {
+  logger.level = Level.FINE;
+  _debugLoggingSubscription = logger.onRecord.listen((record) {
+    // ignore: avoid_print
+    print(record.message);
+  });
+}
+
+void _disableDebugLogging() {
+  logger.level = Level.OFF;
+  _debugLoggingSubscription?.cancel();
+  _debugLoggingSubscription = null;
+}
 
 final logger = Logger.detached('cbl_dart');
 
