adc421555c91ea549cc38279f528d2b9b2e2713f
Gabriel Terwesten <gabriel@terwesten.net>
2024-09-14 20:17:22 +0200

    Remove more references to cbl_dart

--- packages/cbl_generator/test/test_utils.dart
+++ packages/cbl_generator/test/test_utils.dart
@@ -1,18 +1,9 @@
-import 'dart:io';
-
 import 'package:cbl/cbl.dart';
-import 'package:cbl_dart/cbl_dart.dart';
-import 'package:cbl_dart/src/acquire_libraries.dart';
 import 'package:test/scaffolding.dart';
 import 'package:test/test.dart';
 
 Future<void> initCouchbaseLiteForTest() async {
-  final tmpDir = await Directory.systemTemp.createTemp('cbl_generator_test');
-  await setupDevelopmentLibraries();
-  await CouchbaseLiteDart.init(
-    edition: Edition.enterprise,
-    filesDir: tmpDir.path,
-  );
+  await CouchbaseLite.init();
 }
 
 Future<AsyncDatabase> openTestDatabase() async {
